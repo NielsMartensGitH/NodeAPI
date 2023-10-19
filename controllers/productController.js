da440e83f57aca6a4208b70fb402a014fdad8973
NielsMartensMediahuis <niels.martens@mediahuis.be>
2023-10-19 18:06:35 +0200

    github

---
+++ controllers/productController.js
@@ -0,0 +1,78 @@
+const Product = require('../models/productModel');
+const asyncHandler = require('express-async-handler')
+
+
+// get all products
+const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500);
+        throw new Error(error.message);
+    }
+}
+
+// get a single product
+const getProduct = asyncHandler(async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch(error) {
+        res.status(500);
+        throw new Error(error.message);
+    }
+});
+
+// create a product
+const createProduct = asyncHandler(async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch(error) {
+        res.status(500);
+        throw new Error(error.message);
+    }
+});
+
+// update a product
+const updateProduct = asyncHandler(async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        // we cannot find product in database
+        if(!product)
+        { 
+            return res.status(404).json({message: `Cannot find any product with ID ${id}`})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch(error) {
+        res.status(500);
+        throw new Error(error.message);
+    }
+});
+
+// delete a product
+const deleteProduct = asyncHandler(async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product) {
+            return res.status(404).json({message: `Cannot find any product with ID ${id}`})
+        }
+        res.status(200).json();
+    } catch (error) {
+        res.status(500);
+        throw new Error(error.message);
+    }
+});
+
+module.exports = {
+    getProducts, 
+    getProduct, 
+    createProduct, 
+    updateProduct, 
+    deleteProduct
+};
--- server.js
+++ server.js
@@ -1,12 +1,46 @@
+require('dotenv').config()
 const express = require('express');
+const mongoose = require('mongoose');
+const productRoute = require('./routes/productRoute');
+const errorMiddleware = require('./middleware/errorMiddleware');
+const cors = require('cors');
+
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
+const FRONTEND = process.env.FRONTEND;
+
+var corsOptions = {
+    origin: FRONTEND, // frontend url
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 // routes
 
-app.get('/', (req, res) => {
-    res.send('Hello Node API')
-})
+app.use('/api/products', productRoute);
 
-app.listen(3000, () => {
-    console.log('Note API app is running on port 3000');
+app.get('/', (req, res) => {
+    res.send('Hello Node API');
+});
+
+app.get('/blog', (req, res) => {
+    res.send('Hello Log');
+});
+
+app.use(errorMiddleware);
+
+mongoose.set("strictQuery", false);
+mongoose.connect(MONGO_URL)
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Note API app is running on port ${PORT}`);
+    })
+    console.log("connected to MongoDB");
+}).catch((error) => {
+    console.log(error);
 })
